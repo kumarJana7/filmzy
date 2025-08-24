7f7d5b3b18a1619aacba5593a38aace671e4040a
Sravan Jana <sravanjana7@gmail.com>
2025-08-25 03:21:02 +0530

    Seventh Commit

--- src/components/Sprinner.jsx
+++ src/components/Sprinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div role="status">
+        <div role="status" className='flex justify-center items-center'>
             <svg
                 aria-hidden="true"
                 className="h-8 w-8 animate-spin fill-indigo-600 text-gray-200 dark:text-gray-600"
